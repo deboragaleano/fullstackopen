493832fc50a86e8a6fbcedbe569ccc5844631594
deboragaleano <dgaleanoservian@gmail.com>
2020-08-27 18:57:20 +0200

    countries step1

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,5 @@
 import React, {useState, useEffect} from 'react';
 import axios from 'axios'
-import './App.css';
 
 function App() {
   const [countries, setCountries] = useState([])
@@ -8,23 +7,40 @@ function App() {
 
   useEffect(() => {
     axios
-      .get('https://restcountries.eu/rest/v2/all')
+      .get(`https://restcountries.eu/rest/v2/name/${country}`)
       .then(response => {
         setCountries(response.data)
       })
-  }, [])
+  }, [country])
 
   const searchCountry = (e) => {
-    e.preventDefault()
-    // add functionality!! 
+    e.preventDefault();
   }
   
   const handleSearch = (e) => {
     setCountry(e.target.value)
   }
 
-  // const country = countries.map(c => c.name === 'Afghanistan')
-  // console.log(country)
+  const condiDisplay = () => {
+    if(countries.length > 10) {
+      return <p>Too many matches, specify another</p>
+    } else if(countries.length <= 10 && countries.length > 1) {
+      return countries.map(c => <p key={c.alpha2Code}>{c.name}</p>)
+    } else if(countries.length === 1) {
+      const showCountry =
+        countries.map(c => 
+          <div key={c.alpha2Code}>
+            <h1>{c.name}</h1> 
+            <p>capital: {c.capital}</p> 
+            <p>population: {c.population}</p> 
+            <h2>Languages</h2>
+              {c.languages.map(l => <li key={l.iso639_1}>{l.name}</li>)}
+            <img style={{width: '50%'}}src={c.flag} alt={c.name}/>
+          </div>
+        )
+      return showCountry
+    }
+  }
 
   return (
     <div className="App">
@@ -36,8 +52,35 @@ function App() {
             onChange={handleSearch}
           /> 
         </form>
+        {condiDisplay()}
     </div>
   );
 }
 
 export default App;
+
+
+
+  // If there are too many (over 10) countries that match the query, then the user is prompted to make their query more specific:
+  // If there are ten or fewer countries, but more than one, then all countries matching the query are shown:
+  // When there is only one country matching the query, then the basic data of the country, its flag and the languages spoken there, are shown:
+
+  // switch (countries) {
+  //   case countries.length > 10: 
+  //     return <h1>Too many matches, specify another</h1>
+  //   case countries.length <= 10 && countries.length > 1: 
+  //     return <ul>
+  //       {countries.map(c => <li>{c.name}</li>)}
+  //     </ul>
+  //   case countries.length === 1: 
+  //     return <ul>
+  //     {countries.map(c => <li>{c.name} {c.capital}</li>)}
+  //     </ul>
+  //   default: 
+  //     return ''
+  // } 
+
+  // TODO: 
+  // REFACTOR 
+  //	var category = eatsPlants && eatsAnimals ? "omnivore": 
+  // eatsPlants ? "herbivore" : eatsAnimals ? "carnivore" : undefined; 
